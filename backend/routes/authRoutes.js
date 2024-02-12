23cebc2bb5514a74d7eff02ccea98e72cf66c7c4
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2024-02-12 18:05:01 +0530

    finished auth controller

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { Login, Logout, SignUp } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/login", Login)
+
+router.post("/signup", SignUp)
+
+router.post("/logout", Logout)
+
+
+export default router
