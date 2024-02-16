360509e6176bee89b43841cfe0ba717e3302cd33
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2024-02-16 22:12:03 +0530

    finished chat fetching and chat search

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -40,7 +40,7 @@ export const sendMessage = async (req, res) => {
         await Promise.all([conversation.save(), newMessage.save()]);
 
 
-        res.status(201).json({ message: "Message Sent Successfully" });
+        res.status(201).json(newMessage);
 
     } catch (error) {
         console.log("Error in sendMessage controller: ", error.message);
