ec9dba1b9c06d6e1409801e7d12ef94a92828482
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2024-02-19 12:18:40 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,7 @@ dotenv.config();
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 
 app.use(express.json());
@@ -21,6 +23,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World")
